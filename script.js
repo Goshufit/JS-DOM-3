5ed16b00e83ce8f51a7937c11c8257afaae9f2a3
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-14 22:20:33 -0400

    eventListeners

--- script.js
+++ script.js
@@ -32,8 +32,8 @@
 
 
 //.type tells you what event is being listened for//
-let button = document.getElementById('button');
-let box = document.getElementById('box');
+// let button = document.getElementById('button');
+// let box = document.getElementById('box');
 
 //button.addEventListener('click', runEvent);
 //button.addEventListener('dblclick', runEvent);
@@ -43,11 +43,44 @@ let box = document.getElementById('box');
 // box.addEventListener('mouseenter', runEvent);
 // box.addEventListener('mouseleave', runEvent);
 
+//mouseover event is triggered for inner elements and mouse out is triggered when it leaves the parent node//
 // box.addEventListener('mouseover', runEvent);
 // box.addEventListener('mouseout', runEvent);
 
-box.addEventListener('mousemove', runEvent);
+// box.addEventListener('mousemove', runEvent);
 
-function runEvent(e){
-   console.log('EVENT TYPE: '+e.type);
+let itemInput = document.querySelector('input[type="text"]');
+let form = document.querySelector('form');
+let select = document.querySelector('select')
+
+// itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keyup', runEvent);
+// itemInput.addEventListener('keypress', runEvent);
+
+//focus even highlights the text field when it is selected or the form border//
+// itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('blur', runEvent);
+
+// itemInput.addEventListener('cut', runEvent);
+// itemInput.addEventListener('paste', runEvent);
+
+// itemInput.addEventListener('input', runEvent);
+
+// select.addEventListener('change', runEvent)
+// select.addEventListener('input', runEvent)
+
+form.addEventListener('submit', runEvent);
+
+function runEvent(e) {
+   e.preventDefault()
+   console.log('EVENT TYPE: ' + e.type);
+
+   //console.log(e.target.value);
+   // document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3>'
+
+   //Causes a live update of the position of the cursor when the event is being triggered//
+   // output.innerHTML = '<h3>MouseX: '+e.offsetX+'</h3><h3>MouseY: '+e.offsetY+'</h3>'
+
+   //changes the background color of node selected 
+   // document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)"
 }
